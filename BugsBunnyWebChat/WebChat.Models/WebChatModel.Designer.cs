7835293fe1936df2e58c824c279e53471a986d55
Boyan Palagachev <boiankp@gmail.com>
2013-08-14 14:51:34 +0300

    Connection String added

--- BugsBunnyWebChat/WebChat.Models/WebChatModel.Designer.cs
+++ BugsBunnyWebChat/WebChat.Models/WebChatModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\User\Documents\GitHub\BugsBunnyTeam\BugsBunnyWebChat\WebChat.Models\WebChatModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Boyan\Documents\GitHub\BugsBunnyTeam\BugsBunnyWebChat\WebChat.Models\WebChatModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
